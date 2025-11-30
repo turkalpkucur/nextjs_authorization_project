3637864dc3b2b534d474fa889913814e5fcbbcf9
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-11-30 22:13:53 +0300

    214 finish
    214 finish

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,5 +1,9 @@
 'use server';
 
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
 export async function signup(prevState, formData) {
     const email = formData.get('email');
     const password = formData.get('password');
@@ -21,5 +25,19 @@ export async function signup(prevState, formData) {
             errors
         }
     }
-    //store it in the database (create a new user)
+    const hashedPassword= hashUserPassword(password);
+
+    try{
+    createUser(email,hashedPassword);
+    }catch(error){
+        if(error.code==='SQLITE_CONSTRAINT_UNIQUE'){
+            return{
+                errors:{
+                    email:"It seems like an account for the chosen email already exists.."
+                }
+            }
+        }
+        throw error;
+    }
+redirect('/training');
 }
