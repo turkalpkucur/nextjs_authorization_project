8c251d97b74908c72531e08d0c41876929d85ec8
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2025-12-08 01:54:28 +0300

    güncelleme
    güncelleme

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -75,3 +75,12 @@ export async function login(prevState, formData) {
     await createAuthSession(id);
     redirect('/training');
 }
+
+
+export async function auth(mode,prevState,formData){
+    if(mode==='login'){
+        return login(prevState,formData);
+    }
+
+    return signup(prevState,formData);
+}
